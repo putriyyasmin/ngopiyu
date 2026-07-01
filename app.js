5861492d6322ebe72a7a5b86e94bf0dcbb68c2a5
putriyyasmin <putryniayasmin12@gmail.com>
2026-07-01 22:51:07 +0700

    rollback

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import rankRouter from "./src/router/rankRouter.js";
 import errorHandler from "./src/middleware/error.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -19,9 +20,10 @@ app.use(errorHandler);
 db.authenticate()
   .then(() => {
     console.log("Terhubung dengan database");
+    app.listen(PORT, () => {
+      console.log(`Server berjalan di ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("error nih", err.message);
   });
-
-export default app;
