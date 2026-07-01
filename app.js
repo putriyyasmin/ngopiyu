d77b9af2297def29665b94d00fbc748e8627e43f
putriyyasmin <putryniayasmin12@gmail.com>
2026-07-01 23:09:54 +0700

    updated

--- app.js
+++ app.js
@@ -12,18 +12,19 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "Ngopiyu API aktif" });
+});
+
 app.use("/api/cafes", cafeRoutes);
 app.use("/api/rank", rankRouter);
 
 app.use(errorHandler);
 
+app.listen(PORT, () => {
+  console.log(`Server berjalan di ${PORT}`);
+});
+
 db.authenticate()
-  .then(() => {
-    console.log("Terhubung dengan database");
-    app.listen(PORT, () => {
-      console.log(`Server berjalan di ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("error nih", err.message);
-  });
+  .then(() => console.log("Terhubung dengan database"))
+  .catch((err) => console.log("Gagal konek database:", err.message));
