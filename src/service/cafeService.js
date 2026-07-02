efa855ccc0c6933021732b14485158472a16670c
putriyyasmin <putryniayasmin12@gmail.com>
2026-07-02 16:17:25 +0700

    search bisa gabung kata

--- src/service/cafeService.js
+++ src/service/cafeService.js
@@ -7,7 +7,38 @@ const CafeService = {
     const where = {};
 
     if (search) {
-      where.normalisasi_ulasan = { [Op.iLike]: `%${search}%` };
+      const kata = search
+        .toLowerCase()
+        .split(/\s+/)
+        .filter((k) => k);
+
+      if (kata.length > 0) {
+        let idHasil = null;
+        for (const k of kata) {
+          const byNama = await Cafe.findAll({
+            attributes: ["id"],
+            where: { nama_cafe: { [Op.iLike]: `%${k}%` } },
+          });
+          const byUlasan = await Ulasan.findAll({
+            attributes: ["cafe_id"],
+            where: { isi_ulasan: { [Op.iLike]: `%${k}%` } },
+            group: ["cafe_id"],
+          });
+
+          const idKata = new Set([
+            ...byNama.map((c) => c.id),
+            ...byUlasan.map((u) => u.cafe_id),
+          ]);
+
+          idHasil =
+            idHasil === null
+              ? idKata
+              : new Set([...idHasil].filter((id) => idKata.has(id)));
+
+          if (idHasil.size === 0) break;
+        }
+        where.id = { [Op.in]: [...(idHasil ?? [])] };
+      }
     }
     if (kategori) {
       where.kategori = kategori;
